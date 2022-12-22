34c092387cd629241f104137242f3934cce4600e
Raj Shah <rnshah9@asu.edu>
2022-12-22 05:29:52 +0000

    Delete FDP

--- mayhem/fuzz_simplifyPath.cpp
+++ mayhem/fuzz_simplifyPath.cpp
@@ -2,13 +2,13 @@
 #include <stdio.h>
 #include <climits>
 
-#include "FuzzedDataProvider.h"
+#include <fuzzer/FuzzedDataProvider.h>>
 #include "path.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     FuzzedDataProvider provider(data, size);
-    std::string str = provider.ConsumeRandomLengthString();
+    std::string str = provider.ConsumeRandomLengthString(1000);
     Path::simplifyPath(str);
 
     return 0;
